8eb6e8f4e8bac53be5b7bc424670b94a4000a444
filippo <filippo.iacono@abstract.it>
2023-12-08 03:37:54 +0100

    fix crypto

--- src/components/Pages/Crypto/CoinList.tsx
+++ src/components/Pages/Crypto/CoinList.tsx
@@ -33,17 +33,12 @@ export interface CryptoCurrency {
   price_change_percentage_7d_in_currency: number
 }
 
-
 export const CoinList: React.FC = () => {
   const [marketData, setMarketData] = useState<CryptoCurrency[]>([]);
 
   const API_KEY = import.meta.env.VITE_COIN_GEKO_KEY
 
   const fetchMarketData = async () => {
-    // crypto market data
-
-
-
     try {
       fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en&x_cg_demo_api_key=${API_KEY}`)
         .then(response => response.json())
