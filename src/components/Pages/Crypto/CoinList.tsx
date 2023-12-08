568b79e0efd3270b7a11ce7ae450820301558662
filippo <filippo.iacono@abstract.it>
2023-12-08 03:30:54 +0100

    fix crypto table

--- src/components/Pages/Crypto/CoinList.tsx
+++ src/components/Pages/Crypto/CoinList.tsx
@@ -1,33 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import { CoinLogo, CoinName, CoinNameWrap, Table, TableD, TableH, TableR } from './style';
 
 export interface CryptoCurrency {
-  id: string;
-  symbol: string;
-  name: string;
-  image: string;
-  current_price: number;
-  market_cap: number;
-  market_cap_rank: number;
-  fully_diluted_valuation: number;
-  total_volume: number;
-  high_24h: number;
-  low_24h: number;
-  price_change_24h: number;
-  price_change_percentage_24h: number;
-  market_cap_change_24h: number;
-  market_cap_change_percentage_24h: number;
-  circulating_supply: number;
-  total_supply: number;
-  max_supply: number;
-  ath: number;
-  ath_change_percentage: number;
-  ath_date: string;
-  atl: number;
-  atl_change_percentage: number;
-  atl_date: string;
-  roi: null;
-  last_updated: string;
+  id: string
+  symbol: string
+  name: string
+  image: string
+  current_price: number
+  market_cap: number
+  market_cap_rank: number
+  fully_diluted_valuation: number
+  total_volume: number
+  high_24h: number
+  low_24h: number
+  price_change_24h: number
+  price_change_percentage_24h: number
+  market_cap_change_24h: number
+  market_cap_change_percentage_24h: number
+  circulating_supply: number
+  total_supply: number
+  max_supply: number
+  ath: number
+  ath_change_percentage: number
+  ath_date: string
+  atl: number
+  atl_change_percentage: number
+  atl_date: string
+  roi: null
+  last_updated: string
+  price_change_percentage_1h_in_currency: number
+  price_change_percentage_24h_in_currency: number
+  price_change_percentage_7d_in_currency: number
 }
+
 
 export const CoinList: React.FC = () => {
   const [marketData, setMarketData] = useState<CryptoCurrency[]>([]);
@@ -36,8 +41,11 @@ export const CoinList: React.FC = () => {
 
   const fetchMarketData = async () => {
     // crypto market data
+
+
+
     try {
-      fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en&x_cg_demo_api_key=${API_KEY}`)
+      fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en&x_cg_demo_api_key=${API_KEY}`)
         .then(response => response.json())
         .then(data => {
           console.log(data, 'data')
@@ -56,38 +64,50 @@ export const CoinList: React.FC = () => {
   if (!marketData || marketData.length === 0) return <p>No data available</p>;
 
   return (
-    <table>
+    <Table>
       <tr>
-        <th>name</th>
-        <th>symbol/USD</th>
-        <th>current_price</th>
-        <th>market_cap</th>
-        <th>total_volume</th>
-        <th>price_change_percentage_24h</th>
-        <th>circulating_supply</th>
-        <th>total_supply</th>
-        <th>ath</th>
-        <th>ath_change_percentage</th>
-        <th>ath_date</th>
-        <th>last_updated</th>
+        <TableH>#</TableH>
+        <TableH>Name</TableH>
+        <TableH>Price</TableH>
+        <TableH>Price 24</TableH>
+        <TableH>1h</TableH>
+        <TableH>24h</TableH>
+        <TableH>7d</TableH>
+        <TableH>Price 24 %</TableH>
+        <TableH>Market Cup</TableH>
+        <TableH>Total Volume</TableH>
+        <TableH>Cirtulating Supply</TableH>
+        <TableH>Total Supply</TableH>
+        <TableH>ATH</TableH>
+        <TableH>ATH %</TableH>
       </tr>
       {marketData.map(coin => (
-        <tr key={coin.id}>
-          <td>{coin.name}</td>
-          <td>{coin.symbol.toUpperCase()}/USD</td>
-          <td>{coin.current_price}</td>
-          <td>{coin.market_cap}</td>
-          <td>{coin.total_volume}</td>
-          <td>{coin.price_change_percentage_24h}</td>
-          <td>{coin.circulating_supply}</td>
-          <td>{coin.total_supply}</td>
-          <td>{coin.ath}</td>
-          <td>{coin.ath_change_percentage}</td>
-          <td>{coin.ath_date}</td>
-          <td>{coin.last_updated}</td>
-        </tr>
+        <TableR key={coin.id}>
+          <TableD>{coin.market_cap_rank}</TableD>
+          <TableD>
+            <CoinNameWrap>
+              <CoinLogo src={coin.image} alt='coin logo' />
+              <CoinName>
+                {coin.name}
+              </CoinName>
+              {coin.symbol.toUpperCase()}
+            </CoinNameWrap>
+          </TableD>
+          <TableD>$ {coin.current_price.toFixed(2)}</TableD>
+          <TableD>{coin.price_change_24h.toFixed(2)}</TableD>
+          <TableD>{coin.price_change_percentage_1h_in_currency.toFixed(2)}</TableD>
+          <TableD>{coin.price_change_percentage_24h_in_currency.toFixed(2)}</TableD>
+          <TableD>{coin.price_change_percentage_7d_in_currency.toFixed(2)}</TableD>
+          <TableD>{coin.price_change_percentage_24h.toFixed(2)}</TableD>
+          <TableD>{coin.market_cap}</TableD>
+          <TableD>{coin.total_volume}</TableD>
+          <TableD>{coin.circulating_supply}</TableD>
+          <TableD>{coin.total_supply}</TableD>
+          <TableD>{coin.ath}</TableD>
+          <TableD>{coin.ath_change_percentage}</TableD>
+        </TableR>
       ))}
-    </table>
+    </Table>
   )
 }
 
---
+++ src/components/Pages/Crypto/style.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+  border-collapse: collapse;
+`;
+
+export const CoinLogo = styled.img`
+  width: 1.5rem;
+  height: 1.5rem;
+`;
+
+export const TableD = styled.td`
+  padding: 1rem;
+  border: 1px solid #000;
+`;
+
+export const TableH = styled.th`
+  border: 1px solid #000;
+`;
+
+export const TableR = styled.tr`
+  cursor: pointer;
+  text-align: center;
+`;
+
+export const CoinName = styled.span`
+  font-weight: bold;
+  margin-right: 0.5rem;
+`;
+
+export const CoinNameWrap = styled.span`
+  display: flex;
+  align-items: center;
+  text-align: left;
+`;
